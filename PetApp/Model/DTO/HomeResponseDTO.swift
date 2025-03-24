dc7f60a851b1b4435e2c8e9df8334a71ba2bfbb3
jeoungsung12 <jeoungsung12@naver.com>
2025-03-24 19:59:51 +0900

    [Fix] .gitignore

---
+++ PetApp/Model/DTO/HomeResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  HomeResponseDTO.swift
+//  PetApp
+//
+//  Created by 정성윤 on 3/24/25.
+//
+
+import Foundation
+
+struct HomeResponseDTO: Decodable {
+    
+}
---
+++ PetApp/Model/Entity/HomeEntity.swift
@@ -0,0 +1,12 @@
+//
+//  HomeEntity.swift
+//  PetApp
+//
+//  Created by 정성윤 on 3/24/25.
+//
+
+import Foundation
+
+struct HomeEntity {
+    
+}
