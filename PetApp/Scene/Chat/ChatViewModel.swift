47c4d514020f9751de810c6f7dce7e2fbfb4416c
jeoungsung12 <jeoungsung12@naver.com>
2025-03-26 16:26:44 +0900

    [Add] init

---
+++ PetApp/Scene/Chat/ChatViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewModel.swift
+//  PetApp
+//
+//  Created by 정성윤 on 3/26/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class ChatViewModel: BaseViewModel {
+    private var disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+}
+
+extension ChatViewModel {
+    
+    func transform(_ input: Input) -> Output {
+        
+        return Output()
+    }
+}
