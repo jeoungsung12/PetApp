ef4f3b93b0f041750db2ac1baaac6b10e45693a4
jeoungsung12 <jeoungsung12@naver.com>
2025-03-26 16:26:44 +0900

    [Add] init

---
+++ PetApp/Scene/Chat/ChatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatViewController.swift
+//  PetApp
+//
+//  Created by 정성윤 on 3/26/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class ChatViewController: BaseViewController {
+
+    private let viewModel = ChatViewModel()
+    private var disposeBag = DisposeBag()
+    
+    override func configureView() {
+        
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+}
