0444b86571fd34aa231bbbe219442448c87e8927
jeoungsung12 <jeoungsung12@naver.com>
2025-03-31 15:32:37 +0900

    [Add] 채팅 backdrop이미지 추가

--- PetApp/Scene/Chat/ChatViewModel.swift
+++ PetApp/Scene/Chat/ChatViewModel.swift
@@ -17,11 +17,18 @@ enum ChatSectionType: CaseIterable {
 }
 
 final class ChatViewModel: BaseViewModel {
+    private let realmRepo: UserRepositoryType = RealmUserRepository.shared
     private let repository: NetworkRepositoryType = NetworkRepository.shared
     private var disposeBag = DisposeBag()
+    private var likedEntities: [HomeEntity] = []
+    
+    init() {
+        loadRealm()
+    }
     
     struct Input {
         let loadTrigger: Observable<Void>
+        let reloadRealm: PublishRelay<Void>
     }
     
     struct Output {
@@ -29,23 +36,23 @@ final class ChatViewModel: BaseViewModel {
         let errorResult: Driver<ChatError>
     }
     
-}
-
-extension ChatViewModel {
-    
     func transform(_ input: Input) -> Output {
-        let homeResult = PublishRelay<[HomeSection]>()
+        let homeResult = BehaviorRelay<[HomeSection]>(value: [])
         let errorResult = PublishRelay<ChatError>()
         
         input.loadTrigger
-            .flatMapLatest { [weak self] _ -> Single<[HomeSection]> in
+            .withUnretained(self)
+            .flatMapLatest { owner, _ -> Single<[HomeSection]> in
                 return Single<[HomeSection]>.create { single in
                     Task {
                         do {
-                            let result = try await self?.fetchData()
-                            single(.success(result ?? []))
+                            let result = try await owner.fetchData()
+                            single(.success(result))
                         } catch {
-                            single(.failure(error))
+                            if let chatError = error as? ChatError {
+                                errorResult.accept(chatError)
+                            }
+                            single(.success([]))
                         }
                     }
                     return Disposables.create()
@@ -54,30 +61,44 @@ extension ChatViewModel {
             .bind(to: homeResult)
             .disposed(by: disposeBag)
         
+        input.reloadRealm
+            .withUnretained(self)
+            .map { owner, _ in
+                owner.loadRealm()
+                var value = homeResult.value
+                if value.count > 0 {
+                    value[value.count - 1] = HomeSection(title: "관심등록된 친구들 🐾", items: owner.likedEntities.map {
+                        return HomeItem(data: $0)
+                    })
+                }
+                return value
+            }
+            .bind(to: homeResult)
+            .disposed(by: disposeBag)
+        
         return Output(
             homeResult: homeResult.asDriver(onErrorJustReturn: []),
-            errorResult: errorResult.asDriver(onErrorJustReturn: ChatError.unsupportedRegion)
+            errorResult: errorResult.asDriver(onErrorJustReturn: .unsupportedRegion)
         )
     }
     
     private func fetchData() async throws -> [HomeSection] {
-        do {
-            let result = try await repository.getAnimal(1)
-            let firstResult = result.prefix(5)
-            let secondResult = result.dropFirst(5).prefix(5)
-            
-            return [
-                HomeSection(title: "지금 도움이 필요한\n친구들과 대화해 보세요 💬", items: firstResult.map {
-                    return HomeItem(data: $0)
-                }),
-                HomeSection(title: "", items: [.init(data: nil)]),
-                HomeSection(title: "관심등록된 친구들 🐾", items: secondResult.map {
-                    return HomeItem(data: $0)
-                })
-            ]
-            
-        } catch {
-            throw error
+        let result = try await repository.getAnimal(1)
+        
+        return [
+            HomeSection(title: "지금 도움이 필요한\n친구들과 대화해 보세요 💬", items: result.map {
+                return HomeItem(data: $0)
+            }),
+            HomeSection(title: "", items: [.init(data: nil)]),
+            HomeSection(title: "관심등록된 친구들 🐾", items: likedEntities.map {
+                return HomeItem(data: $0)
+            })
+        ]
+    }
+    
+    private func loadRealm() {
+        DispatchQueue.main.async { [weak self] in
+            self?.likedEntities = self?.realmRepo.getAllLikedHomeEntities() ?? []
         }
     }
 }
