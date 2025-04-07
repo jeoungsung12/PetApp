5db6dfceb28813e693917703b49dd2278e0e9514
jeoungsung12 <jeoungsung12@naver.com>
2025-04-08 08:50:01 +0900

    [Fix] MapVC-UserLocation

--- PetApp/App/Coordinator/Coordinator.swift
+++ PetApp/App/Coordinator/Coordinator.swift
@@ -7,18 +7,18 @@
 
 import Foundation
 
-protocol Coordinator: AnyObject {
-    var childCoordinators: [Coordinator] { get set }
-    var navigationController: UINavigationController { get set }
-    
-    func start()
-    func childDidFinish(_ child: Coordinator?)
-}
-
-extension Coordinator {
-    func childDidFinish(_ child: Coordinator?) {
-        if let child = child, let index = childCoordinators.firstIndex(where: { $0 === child }) {
-            childCoordinators.remove(at: index)
-        }
-    }
-}
+//protocol Coordinator: AnyObject {
+//    var childCoordinators: [Coordinator] { get set }
+//    var navigationController: UINavigationController { get set }
+//    
+//    func start()
+//    func childDidFinish(_ child: Coordinator?)
+//}
+//
+//extension Coordinator {
+//    func childDidFinish(_ child: Coordinator?) {
+//        if let child = child, let index = childCoordinators.firstIndex(where: { $0 === child }) {
+//            childCoordinators.remove(at: index)
+//        }
+//    }
+//}
---
+++ PetApp/App/Coordinator/Scene/TabBarCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarCoordinator.swift
+//  PetApp
+//
+//  Created by 정성윤 on 4/7/25.
+//
+
+import UIKit
+
+//final class TabBarController: UITabBarController {
+//    
+//    weak var coordinator: AppCoordinator?
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        configure()
+//    }
+//    
+//    private func configure() {
+//        let appearance = UITabBarAppearance()
+//        appearance.configureWithOpaqueBackground()
+//        appearance.backgroundColor = .customWhite
+//        self.tabBar.standardAppearance = appearance
+//        
+//        self.selectedIndex = 0
+//        self.tabBar.tintColor = .point
+//        self.tabBar.unselectedItemTintColor = .customLightGray
+//    }
+//}
