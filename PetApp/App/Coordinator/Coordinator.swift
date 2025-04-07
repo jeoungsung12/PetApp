8bdbbb326f79ad3f22f667af3122ddc9df308aa7
jeoungsung12 <jeoungsung12@naver.com>
2025-04-08 08:53:13 +0900

    [Add] AppCoordinator

--- PetApp/App/Coordinator/Coordinator.swift
+++ PetApp/App/Coordinator/Coordinator.swift
@@ -5,20 +5,20 @@
 //  Created by 정성윤 on 4/7/25.
 //
 
-import Foundation
+import UIKit
 
-//protocol Coordinator: AnyObject {
-//    var childCoordinators: [Coordinator] { get set }
-//    var navigationController: UINavigationController { get set }
-//    
-//    func start()
-//    func childDidFinish(_ child: Coordinator?)
-//}
-//
-//extension Coordinator {
-//    func childDidFinish(_ child: Coordinator?) {
-//        if let child = child, let index = childCoordinators.firstIndex(where: { $0 === child }) {
-//            childCoordinators.remove(at: index)
-//        }
-//    }
-//}
+protocol Coordinator: AnyObject {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+    func childDidFinish(_ child: Coordinator?)
+}
+
+extension Coordinator {
+    func childDidFinish(_ child: Coordinator?) {
+        if let child = child, let index = childCoordinators.firstIndex(where: { $0 === child }) {
+            childCoordinators.remove(at: index)
+        }
+    }
+}
